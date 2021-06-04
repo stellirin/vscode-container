ebd98752dbf2b1d0cc142851b56dacc982a8b8e7
Adam Farden <adam@farden.cz>
2021-06-04 13:08:30 +0200

    Add Java, TinyGo

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_VER=v0.166.1
+SCRIPT_VER=v0.181.0
 
 docker pull ubuntu:focal
 
@@ -12,25 +12,19 @@ docker build \
 docker build \
     --file=golang/Dockerfile \
     --build-arg=SCRIPT_VER=${SCRIPT_VER} \
-    --build-arg=TARGET_GO_VERSION=1.16.3 \
+    --build-arg=TARGET_GO_VERSION=1.16.5 \
     --tag=ghcr.io/stellirin/vscode-container:golang-1.16 .
 
 docker build \
     --file=node/Dockerfile \
     --build-arg=SCRIPT_VER=${SCRIPT_VER} \
-    --build-arg=NODE_VERSION=12.18.2 \
-    --tag=ghcr.io/stellirin/vscode-container:node-12 .
-
-docker build \
-    --file=node/Dockerfile \
-    --build-arg=SCRIPT_VER=${SCRIPT_VER} \
-    --build-arg=NODE_VERSION=14.16.0 \
+    --build-arg=NODE_VERSION=14.16.1 \
     --tag=ghcr.io/stellirin/vscode-container:node-14 .
 
 docker build \
     --file=golang+node/Dockerfile \
     --build-arg=SCRIPT_VER=${SCRIPT_VER} \
-    --build-arg=NODE_VERSION=14.16.0 \
+    --build-arg=NODE_VERSION=14.16.1 \
     --tag=ghcr.io/stellirin/vscode-container:golang-1.16_node-14 .
 
 docker build \
@@ -38,3 +32,9 @@ docker build \
     --build-arg=SCRIPT_VER=${SCRIPT_VER} \
     --build-arg=TERRAFORM_VERSION=0.14.7 \
     --tag=ghcr.io/stellirin/vscode-container:terraform-0.14.7 .
+
+docker build \
+    --file=java/Dockerfile \
+    --build-arg=SCRIPT_VER=${SCRIPT_VER} \
+    --build-arg=JAVA_VERSION=8.0.282.j9-adpt \
+    --tag=ghcr.io/stellirin/vscode-container:java-8 .
